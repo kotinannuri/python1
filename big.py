90263d42fcd51d56e77b2ac3585cd226714141b4
KOTESWARA RAO <nannuri.rao@sonata-software.com>
2021-11-06 21:48:04 +0530

    all files are added

---
+++ big.py
@@ -0,0 +1,10 @@
+a=int(input("enter any value"))
+a=int(input("enter any value"))
+c=int(input("enter any value"))
+if (a>b) and (a>c):
+    print("a is big")
+elif(b>a) and (b>c):
+    print("b is big")
+else:
+    print("c is big")
+    print("the big is",big)
